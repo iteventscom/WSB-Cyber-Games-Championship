f0951516811dcf67ec397b3c91f32348e9901a22
zakharych <alexzlk@yandex.ru>
2021-02-03 15:58:58 +0300

    feat: start chache theme

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
 let hero__btns = document.querySelector('.hero__btns');
 let hero__line = document.querySelector('.hero__line');
-let hero__section = document.querySelector('.hero-section')
+let hero__section = document.querySelector('.hero-section');
+let section__titles = document.querySelectorAll('.section__title');
+let buttons = document.querySelectorAll('.button');
+let format__items = document.querySelectorAll('.format__item');
+let hero__subtitle = document.querySelector('.hero__subtitle');
+let subject__section = document.querySelector('#subjects').closest('section');
+console.log(format__items);
 
 hero__btns.addEventListener('click', (event) => {
   event.preventDefault();
@@ -9,6 +15,7 @@ hero__btns.addEventListener('click', (event) => {
   }
 });
 
+let prevTheme = '';
 function changeBtn(btn) {
   btns = [...hero__btns.children];
   btns.forEach((element) => {
@@ -18,7 +25,33 @@ function changeBtn(btn) {
 
   hero__line.className = 'hero__line';
   hero__line.classList.add(`hero__line--${btn.id}`);
-  
+
   hero__section.className = 'section section--margin hero-section';
   hero__section.classList.add(`hero-section--${btn.id}`);
+
+  section__titles.forEach((element) => {
+    element.classList.remove(`section__title--${prevTheme}`);
+    element.classList.add(`section__title--${btn.id}`);
+  });
+
+  buttons.forEach((element) => {
+    element.classList.remove(`button--${prevTheme}`);
+    element.classList.add(`button--${btn.id}`);
+  });
+
+  format__items.forEach((element) => {
+    element.classList.remove(`format__item--${prevTheme}`);
+    element.classList.add(`format__item--${btn.id}`);
+  });
+
+  hero__subtitle.classList.remove(`hero__subtitle--${prevTheme}`);
+  hero__subtitle.classList.add(`hero__subtitle--${btn.id}`);
+
+  if (btn.id !== 'main') {
+    subject__section.classList.add('section--invisible');
+  } else {
+    subject__section.classList.remove('section--invisible');
+  }
+
+  prevTheme = btn.id;
 }
