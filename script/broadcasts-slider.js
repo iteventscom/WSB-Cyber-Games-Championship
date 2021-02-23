fd21796bcad56a969ce89d66ffe9eecc973bc9ba
Alex <alexzlkl@yandex.ru>
2021-02-23 22:15:30 +0300

    feat: replace video by click

--- script/broadcasts-slider.js
+++ script/broadcasts-slider.js
@@ -4,16 +4,25 @@ let activeSlide = Array.from(slides.children)[3];
 
 slider.addEventListener('click', (event) => {
   let videoLink = slides.children[1].firstElementChild.getAttribute('adr');
-  if (event.target.id === 'broadcasts__arrow-down') {
-    event.preventDefault();
-    slides.prepend(slides.lastElementChild);
-    videoLink = slides.children[1].firstElementChild.getAttribute('adr');
-    broadcastplayer.loadVideoById(videoLink);
-  } else if (event.target.id === 'broadcasts__arrow-up') {
-    event.preventDefault();
-    slides.append(slides.firstElementChild);
-    videoLink = slides.children[1].firstElementChild.getAttribute('adr');
-
-    broadcastplayer.loadVideoById(videoLink);
+  if (event.target !== slider && event.target !== slides) {
+    if (event.target.id === 'broadcasts__arrow-down') {
+      event.preventDefault();
+      slides.prepend(slides.lastElementChild);
+      videoLink = slides.children[1].firstElementChild.getAttribute('adr');
+      broadcastplayer.loadVideoById(videoLink);
+    } else if (event.target.id === 'broadcasts__arrow-up') {
+      event.preventDefault();
+      slides.append(slides.firstElementChild);
+      videoLink = slides.children[1].firstElementChild.getAttribute('adr');
+      broadcastplayer.loadVideoById(videoLink);
+    } else if (event.target.closest('.broadcasts-slider__item')) {
+      console.log(slides.children[1]);
+      if (slides.children[1] !== event.target.closest('.broadcasts-slider__item')) {
+        videoLink = event.target.closest('.broadcasts-slider__link').getAttribute('adr');
+        broadcastplayer.loadVideoById(videoLink);
+        
+      slides.prepend(slides.lastElementChild);
+      }
+    }
   }
 });
